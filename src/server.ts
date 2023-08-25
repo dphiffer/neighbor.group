89e92218836e91e9acd0927721f60528d3ea2c15
Dan Phiffer <dan@phiffer.org>
2023-08-25 03:27:12 -0400

    use port env var

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
+import dotenv from "dotenv";
 import buildApp from "./app";
 
-const app = buildApp();
-
+dotenv.config();
 let host = process.env.HOST ? process.env.HOST : "0.0.0.0";
 let port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
+const app = buildApp();
 app.listen({
 	host: host,
 	port: port,
