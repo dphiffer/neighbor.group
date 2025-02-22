bb8bdfb94ce9d10ee3922e0783d3ad0585baabd6
Dan Phiffer <dan@phiffer.org>
2025-02-22 14:04:47 -0600

    add db.groups queries

---
+++ src/db/groups.ts
@@ -0,0 +1,105 @@
+import { Database } from "better-sqlite3";
+
+export interface GroupsRow {
+	id: number;
+	name: string;
+	slug: string;
+	description: string;
+	active?: number;
+	created?: Date;
+	updated?: Date;
+}
+
+export default class GroupsQueries {
+	db: Database;
+	
+	constructor(db: Database) {
+		this.db = db;
+	}
+
+	all() {
+		let stmt = this.db.prepare(`
+			SELECT *
+			FROM groups
+			WHERE active = 1
+		`);
+		return stmt.all() as GroupsRow[];
+	}
+
+	select(id: number) {
+		let stmt = this.db.prepare(`
+			SELECT *
+			FROM groups
+			WHERE id = ?
+		`);
+		let row = stmt.get(id) as GroupsRow | null;
+		if (!row) {
+			return null;
+		}
+		return row;
+	}
+
+	loadBySlug(slug: string) {
+		let stmt = this.db.prepare(`
+			SELECT *
+			FROM groups
+			WHERE slug = ?
+			AND active = 1
+		`);
+		let row = stmt.get(slug) as GroupsRow | null;
+		if (!row) {
+			return null;
+		}
+		return row;
+	}
+
+	insert(values: Partial<GroupsRow>) {
+		const cols = [];
+		const placeholders = [];
+		const skipCols = ["id", "created", "updated"];
+		for (const col of Object.keys(values)) {
+			if (skipCols.indexOf(col) === -1) {
+				cols.push(col);
+				placeholders.push(`$${col}`);
+			}
+		}
+		console.log(values);
+		console.log(cols);
+		console.log(placeholders);
+		let stmt = this.db.prepare(`
+			INSERT INTO groups
+			(${cols.join(", ")}, created, updated)
+			VALUES (${placeholders.join(", ")}, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+		`);
+		return stmt.run({ ... values});
+	}
+
+	update(id: number, values: Partial<GroupsRow>) {
+		const assignments = [];
+		const skipCols = ["id", "created", "updated"];
+		for (const col of Object.keys(values)) {
+			if (skipCols.indexOf(col) === -1) {
+				assignments.push(`${col} = $${col}`);
+			}
+		}
+		let stmt = this.db.prepare(`
+			UPDATE groups
+			SET ${assignments.join(", ")},
+				updated = CURRENT_TIMESTAMP
+			WHERE id = $id
+		`);
+		return stmt.run({
+			id: id,
+			...values,
+		});
+	}
+
+	delete(id: number) {
+		let stmt = this.db.prepare(`
+			UPDATE groups
+			SET active = 0
+			WHERE id = ?
+		`);
+		return stmt.run(id);
+	}
+};
