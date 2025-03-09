77b34c89d2ea5dd6002b05041d7d20c52ae667cd
Dan Phiffer <dan@phiffer.org>
2025-03-09 15:36:52 -0400

    add db.groupMember queries

---
+++ src/db/groupMember.ts
@@ -0,0 +1,45 @@
+import { Database } from "better-sqlite3";
+
+export interface GroupMemberRow {
+	group_id: number;
+	user_id: number;
+	created?: Date;
+}
+
+export default class GroupMemberQueries {
+	db: Database;
+	
+	constructor(db: Database) {
+		this.db = db;
+	}
+
+	insert(values: GroupMemberRow) {
+		let stmt = this.db.prepare(`
+			INSERT INTO group_member
+			(group_id, user_id, created)
+			VALUES ($user_id, $group_id, CURRENT_TIMESTAMP)
+		`);
+		return stmt.run({ ... values});
+	}
+
+	select(groupId: number, userId: number) {
+		let stmt = this.db.prepare(`
+			SELECT *
+			FROM group_member
+			WHERE group_id = ?
+			  AND user_id = ?
+		`);
+		return stmt.get(groupId, userId) as GroupMemberRow | null;
+	}
+
+	delete(groupId: number, userId: number) {
+		let stmt = this.db.prepare(`
+			DELETE
+			FROM group_member
+			WHERE group_id = ?
+			  AND user_id = ?
+		`);
+		stmt.run(groupId, userId);
+	}
+
+}
