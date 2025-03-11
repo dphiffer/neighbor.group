56ce8588d4b8c7e1190839aa010d1f8c98249031
Dan Phiffer <dan@phiffer.org>
2025-03-11 06:29:02 -0400

    add group membership routes and views

--- src/routes/group.ts
+++ src/routes/group.ts
@@ -24,7 +24,14 @@ export default (
 
 	app.post('/new', (request: FastifyRequest<{ Body: GroupsRow }>, reply) => {
 		try {
+			if (!request.user) {
+				return reply.status(401).view('error.njk', {
+					status: 401,
+					feedback: 'Sorry you are not authorized to create a new group.',
+				});
+			}
 			const group = GroupModel.create(app.db, request.body);
+			group.addMember(app.db, request.user.data.id);
 			return reply.redirect(`/${group.data.slug}`);
 		} catch (err) {
 			let feedback = "Error: something unexpected happened.";
@@ -46,9 +53,15 @@ export default (
 		try {
 			if (!request.user) {
 				let redirect = encodeURIComponent(`/${request.params.group}`);
-				reply.redirect(`/login?redirect=${redirect}`);
+				return reply.redirect(`/login?redirect=${redirect}`);
 			}
 			const group = GroupModel.load(app.db, request.params.group);
+			if (!group.hasMember(app.db, request.user.data.id)) {
+				let redirect = encodeURIComponent(`/${request.params.group}`);
+				return reply.code(403).view('group/join.njk', {
+					group: group
+				});
+			}
 			const messages = MessageModel.query(app.db, group.data.id);
 			return reply.view('group/index.njk', {
 				title: group.data.name,
@@ -98,6 +111,102 @@ export default (
 				feedback = err.message;
 				if (feedback.substring(0, 20) == 'Could not find group') {
 					status = 404;
+				}
+			}
+			return reply.code(status).view("error.njk", {
+				status: status,
+				feedback: feedback
+			});
+		}
+	});
+
+	app.get('/:group/join', (request: FastifyRequest<{
+		Params: {
+			group: string
+		}
+	}>, reply) => {
+		const group = GroupModel.load(app.db, request.params.group);
+		if (!group) {
+			return reply.code(404).view("error.njk", {
+				status: 404,
+				feedback: 'Page not found.'
+			});
+		}
+		return reply.view('group/join.njk', {
+			group: group,
+		});
+	});
+
+	app.post('/:group/join', (request: FastifyRequest<{
+		Params: {
+			group: string
+		}
+	}>, reply) => {
+		try {
+			if (!request.user) {
+				throw new Error('You must be signed in to join group.');
+			}
+			const group = GroupModel.load(app.db, request.params.group);
+			if (!group.hasMember(app.db, request.user.data.id)) {
+				group.addMember(app.db, request.user.data.id);
+			}
+			return reply.redirect(`/${group.data.slug}`);
+		} catch (err) {
+			let status = 500;
+			let feedback = "Something unexpected happened.";
+			if (err instanceof Error) {
+				status = 400;
+				feedback = err.message;
+				if (feedback == 'You must be signed in to join group.') {
+					status = 401;
+				}
+			}
+			return reply.code(status).view("error.njk", {
+				status: status,
+				feedback: feedback
+			});
+		}
+	});
+
+	app.get('/:group/leave', (request: FastifyRequest<{
+		Params: {
+			group: string
+		}
+	}>, reply) => {
+		const group = GroupModel.load(app.db, request.params.group);
+		if (!group) {
+			return reply.code(404).view("error.njk", {
+				status: 404,
+				feedback: 'Page not found.'
+			});
+		}
+		return reply.view('group/leave.njk', {
+			group: group,
+		});
+	});
+
+	app.post('/:group/leave', (request: FastifyRequest<{
+		Params: {
+			group: string
+		}
+	}>, reply) => {
+		try {
+			if (!request.user) {
+				throw new Error('You must be signed in to leave group.');
+			}
+			const group = GroupModel.load(app.db, request.params.group);
+			if (group.hasMember(app.db, request.user.data.id)) {
+				group.removeMember(app.db, request.user.data.id);
+			}
+			return reply.redirect('/');
+		} catch (err) {
+			let status = 500;
+			let feedback = "Something unexpected happened.";
+			if (err instanceof Error) {
+				status = 400;
+				feedback = err.message;
+				if (feedback == 'You must be signed in to leave group.') {
+					status = 401;
 				}
 			}
 			return reply.code(status).view("error.njk", {
